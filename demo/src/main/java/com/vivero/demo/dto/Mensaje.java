3fcd9d7655f342568fb5a3c29f10b394123cb5a3
Martin Fabregues <martinfabregues@hotmail.com>
2023-01-20 11:29:23 -0300

    back

---
+++ demo/src/main/java/com/vivero/demo/dto/Mensaje.java
@@ -0,0 +1,18 @@
+package com.vivero.demo.dto;
+
+//mensaje que se muestra
+public class Mensaje {
+    private  String mensaje;
+
+    public Mensaje(String mensaje){
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje(){
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+}
---
+++ demo/src/main/java/com/vivero/demo/repository/ProductoRepository.java
@@ -0,0 +1,18 @@
+package com.vivero.demo.repository;
+
+import com.vivero.demo.entity.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Long> {
+
+
+        Optional<Producto> findByNombre(String nombre);
+
+        boolean existsByNombre(String nombre);
+    }
+
+
